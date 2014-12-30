b48c94e6c2945502bd1aa1c827e02d5b4d9049bc
Davi Zonete <davi@gatec.com.br>
2014-12-30 17:00:47 -0200

    First

---
+++ FormsTeste/Form2.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FormsTeste
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCarregaSelecionado = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(504, 179);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnCarregaSelecionado
+            // 
+            this.btnCarregaSelecionado.Location = new System.Drawing.Point(193, 211);
+            this.btnCarregaSelecionado.Name = "btnCarregaSelecionado";
+            this.btnCarregaSelecionado.Size = new System.Drawing.Size(148, 41);
+            this.btnCarregaSelecionado.TabIndex = 1;
+            this.btnCarregaSelecionado.Text = "Carregar Selecionado";
+            this.btnCarregaSelecionado.UseVisualStyleBackColor = true;
+            this.btnCarregaSelecionado.Click += new System.EventHandler(this.btnCarregaSelecionado_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 262);
+            this.Controls.Add(this.btnCarregaSelecionado);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCarregaSelecionado;
+    }
+}
---
+++ FormsTeste/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsTeste
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            /*Carregar Campos na GRID*/
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("Id", typeof (Int16)));
+            dt.Columns.Add(new DataColumn("Nome", typeof(String)));
+            var row = dt.NewRow();
+            row["Id"] = 1;
+            row["Nome"] = "Nome 1";
+            dt.Rows.Add(row);
+
+            row = dt.NewRow();
+            row["Id"] = 2;
+            row["Nome"] = "Nome 2";
+            dt.Rows.Add(row);
+
+            row = dt.NewRow();
+            row["Id"] = 3;
+            row["Nome"] = "Nome 3";
+            dt.Rows.Add(row);
+
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void btnCarregaSelecionado_Click(object sender, EventArgs e)
+        {
+var form = Application.OpenForms.OfType<Form1>().First(); // Busca o Form Pai Aberto  
+if (form != null) // Caso encontrar o Pai preenche os campos 
+{
+    //caso nao estiver selecionado nenhum deixa vazio
+    form.txtForm1.Text = dataGridView1.CurrentRow != null? dataGridView1.CurrentRow.Cells["Id"].Value.ToString() :"";
+    form.txt2Form1.Text = dataGridView1.CurrentRow != null? dataGridView1.CurrentRow.Cells["Nome"].Value.ToString():""; 
+}
+this.Close();//Fecha o Form Filho
+        }
+    }
+}
